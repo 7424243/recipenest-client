48a4b8941501af5b2cd48d603f5963df8bbec74e
Sam Sheets <sam.sheets94@gmail.com>
2021-01-20 11:31:58 -0700

    implemented dummyStore data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+
+import React, {Component} from 'react'
+import {Route, Link} from 'react-router-dom'
+import dummyStore from './dummy-store'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+
+  componentDidMount() {
+    this.setState(dummyStore)
+  }
+
+  render () {
+    const {recipes} = this.state
+    return(
+      <p>Hello world!</p>
+    )
+  }
 }
 
-export default App;
+
+
+export default App
