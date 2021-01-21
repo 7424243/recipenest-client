d23800534a6ef84976e537e2a5d55580164687e7
Sam Sheets <sam.sheets94@gmail.com>
2021-01-21 14:23:40 -0700

    fixed LandingPage li styling

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -9,13 +9,13 @@ function LandingPage() {
             <p>RecipeNest is a platform created to help keep track of virtual recipes.</p> 
             <p>It allows you to...</p>
             <ul>
-                <li>Save links and notes for recipes.</li>
-                <li>Get all previously saves recipes.</li>
+                <li className='landing-page-li'>Save links and notes for recipes.</li>
+                <li className='landing-page-li'>Get all previously saves recipes.</li>
             </ul>
             <p>Initially, you are shown a list of previously saved recipe names and a fun word cloud that was created based on the recipe entry's description.</p> 
             <p>To view a saved recipe, click on the recipe name. </p>
             <p>To add a new recipe, click on 'Add Recipe' and you will be prompted to provide some details such as the name of the recipe, the link, a description, and any additional notes you want.</p>
-            <Link to='/recipes'><button>Get Started!</button></Link>
+            <Link className='landing-page-button' to='/recipes'><button>Get Started!</button></Link>
         </div>
     )
 }
