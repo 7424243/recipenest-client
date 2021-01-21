b9cfa919a65588c4829fd5cd9a84b46a2b9ddde9
Sam Sheets <sam.sheets94@gmail.com>
2021-01-21 12:02:58 -0700

    Header and NavBar styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import NavBar from '../NavBar/NavBar'
+import './Header.css'
 
 
 function Header() {
         return (
-            <>
+            <div className='header-container'>
                 <Link to='/recipes'><h1>RecipeNest</h1></Link>
                 <NavBar />
-            </>
+            </div>
         )
 }
 
--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -6,8 +6,15 @@ import './LandingPage.css'
 function LandingPage() {
     return (
         <div className='landing-page-container'>
-            <p>RecipeNest is a platform to help keep track of virtual recipes. It allows you to save links and notes for recipes that you have tried. </p>
-            <p>Initially, you are shown a list of previously saved recipe names and a fun word cloud that was created based on the recipe entry's description. To view a saved recipe, click on the recipe name. To add a new recipe, click on 'Add Recipe' and you will be prompted to provide some details such as the name of the recipe, the link, a description, and any additional notes you want.</p>
+            <p>RecipeNest is a platform created to help keep track of virtual recipes.</p> 
+            <p>It allows you to...</p>
+            <ul>
+                <li>Save links and notes for recipes.</li>
+                <li>Get all previously saves recipes.</li>
+            </ul>
+            <p>Initially, you are shown a list of previously saved recipe names and a fun word cloud that was created based on the recipe entry's description.</p> 
+            <p>To view a saved recipe, click on the recipe name. </p>
+            <p>To add a new recipe, click on 'Add Recipe' and you will be prompted to provide some details such as the name of the recipe, the link, a description, and any additional notes you want.</p>
             <Link to='/recipes'><button>Get Started!</button></Link>
         </div>
     )
