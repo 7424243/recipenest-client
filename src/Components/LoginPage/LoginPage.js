c3349df0b85b3367b4423983b451c0b616767415
Sam Sheets <sam.sheets94@gmail.com>
2021-01-21 12:41:32 -0700

    LoginPage styling

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,22 +1,23 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import './LoginPage.css'
 
 class LoginPage extends Component {
     render() {
         return (
             <>
                 <h3>Login!</h3>
-                <form action="">
-                    <section className="username">
-                        <label htmlFor="username">Username: </label>
-                        <input type="text" placeholder="username" />
+                <form className='login-form'>
+                    <section className='username'>
+                        <label htmlFor='username'>Username: </label>
+                        <input type='text' placeholder='username' />
                     </section>
-                    <section className="password">
-                        <label htmlFor="password">Password: </label>
-                        <input type="text" placeholder="password" />
+                    <section className='password'>
+                        <label htmlFor='password'>Password: </label>
+                        <input type='text' placeholder='password' />
                     </section>
-                    <section className="buttons">
-                        <Link to="/recipes"><button>Cancel</button></Link>
+                    <section className='buttons'>
+                        <Link to='/recipes'><button>Cancel</button></Link>
                         <button>Submit</button>
                     </section>
                 </form>
