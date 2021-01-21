163a6b595cd31a29e1789f2354d070aa77dfc379
Sam Sheets <sam.sheets94@gmail.com>
2021-01-21 15:18:02 -0700

    styling of bullet points on landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -8,14 +8,17 @@ function LandingPage() {
         <div className='landing-page-container'>
             <p>RecipeNest is a platform created to help keep track of virtual recipes.</p> 
             <p>It allows you to...</p>
-            <ul>
+            <ul className='landing-page-list'>
                 <li className='landing-page-li'>Save links and notes for recipes.</li>
-                <li className='landing-page-li'>Get all previously saves recipes.</li>
+                <li className='landing-page-li'>Get all previously saved recipes.</li>
             </ul>
             <p>Initially, you are shown a list of previously saved recipe names and a fun word cloud that was created based on the recipe entry's description.</p> 
             <p>To view a saved recipe, click on the recipe name. </p>
             <p>To add a new recipe, click on 'Add Recipe' and you will be prompted to provide some details such as the name of the recipe, the link, a description, and any additional notes you want.</p>
-            <Link className='landing-page-button' to='/recipes'><button>Get Started!</button></Link>
+            <div className='landing-page-button-container'>
+                <Link  to='/recipes'><button className='landing-page-button'>Get Started!</button></Link>
+            </div>
+            
         </div>
     )
 }
