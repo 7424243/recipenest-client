f8ce42c394304e5ebf3ebc17ea5208b44f650cae
Sam Sheets <sam.sheets94@gmail.com>
2021-01-27 09:47:30 -0700

    implemented TokenService object for LoginPage

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,25 +1,63 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import TokenService from '../../services/token-service'
 import './LoginPage.css'
 
 class LoginPage extends Component {
+
+    state = {
+        user_name: '',
+        password: ''
+    }
+
+    handleSubmitBasicAuth = e => {
+        e.preventDefault()
+        const {user_name, password} = this.state
+        TokenService.saveAuthToken(
+            TokenService.makeBasicAuthToken(user_name, password)
+        )
+        this.props.history.push('/recipes')
+    }
+
+    handleUsername = e => {
+        this.setState({user_name: e.target.value})
+    }
+
+    handlePassword = e => {
+        this.setState({password: e.target.value})
+    }
+
+
     render() {
         return (
             <>
                 <h3>Login!</h3>
-                <form className='login-form'>
+                <form 
+                    className='login-form'
+                    onSubmit={this.handleSubmitBasicAuth}
+                >
                     <section className='username'>
                         <label htmlFor='username'>Username: </label>
-                        <input type='text' placeholder='username' />
+                        <input 
+                            type='text' 
+                            placeholder='username' 
+                            required
+                            onChange={this.handleUsername}
+                        />
                     </section>
                     <section className='password'>
                         <label htmlFor='password'>Password: </label>
-                        <input type='text' placeholder='password' />
+                        <input 
+                            type='text' 
+                            placeholder='password' 
+                            required
+                            onChange={this.handlePassword}
+                        />
                     </section>
                     <section className='buttons'>
                         <Link to='/recipes'><button>Cancel</button></Link>
                         <Link to='/signup'><button>Sign Up</button></Link>
-                        <button>Submit</button>
+                        <button type='submit'>Submit</button>
                     </section>
                 </form>
             </>
