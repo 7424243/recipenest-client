e4eccbfdecc22fcb7fc058f95aadefe80f1e8fd4
Sam Sheets <sam.sheets94@gmail.com>
2021-01-27 09:54:40 -0700

    implemented Login/Logout conditional render in header

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import TokenService from '../../services/token-service'
 import './NavBar.css'
 
-function NavBar() {
-    return (
-        <nav>
-            <Link to='/recipes' className='nav-link'>All Recipes |</Link>
-            <Link to='/recipes' className='nav-link'> My Recipes |</Link>
+class NavBar extends Component {
+
+    logoutLink() {
+        return (
+            <Link to='/' className='nav-link'> Logout </Link>
+        )
+    }
+
+    loginLink() {
+        return (
             <Link to='/login' className='nav-link'> Login </Link>
-        </nav>
-    )
+        )
+    }
+    
+    render() {
+        return (
+            <nav>
+                <Link to='/recipes' className='nav-link'>All Recipes |</Link>
+                <Link to='/recipes' className='nav-link'> My Recipes |</Link>
+                {TokenService.hasAuthToken() ? this.logoutLink() : this.loginLink()}
+                
+            </nav>
+        )
+    }
+
 }
 
 export default NavBar
