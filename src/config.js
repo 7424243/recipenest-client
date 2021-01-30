c98734c064d0ae9b73f089a004ae420563a042e9
Sam Sheets <sam.sheets94@gmail.com>
2021-01-30 10:16:44 -0700

    updated api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_ENDPOINT: `postgres://cbetfwmxdhukkz:ea7fd7d584433c1ce31924fbd932a878db6bc896331a514941370d476df0934a@ec2-52-72-162-207.compute-1.amazonaws.com:5432/dc2ccumvh7jt67`,
+    API_ENDPOINT: `https://infinite-headland-60692.herokuapp.com/api`,
     TOKEN_KEY: 'recipenest-client-auth-token'
 }
 
