cfe5a033c94354311ba7059b45fb9d840e9fde47
Sam Sheets <sam.sheets94@gmail.com>
2021-02-03 13:38:13 -0700

    replaced API_ENDPOINT in config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
 
     
-    API_ENDPOINT: `http://localhost:8000/api`,
+    API_ENDPOINT: `https://infinite-headland-60692.herokuapp.com/api`,
 
     TOKEN_KEY: 'recipenest-client-auth-token'
 }
@@ -9,6 +9,6 @@ const config = {
 export default config
 
 
-//
+//local host API_ENDPOINT: `http://localhost:8000/api`,
 
-//API_ENDPOINT: `https://infinite-headland-60692.herokuapp.com/api`,
+//
