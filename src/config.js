bb3ac8781464a6d3a8c671c5a5a6b6b491b6ca1f
Sam Sheets <sam.sheets94@gmail.com>
2021-02-03 13:29:44 -0700

    moved manifest link in index.html

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = {
 
     
-    API_ENDPOINT: `http://localhost:8000/api`,
-
+    
+    API_ENDPOINT: `https://infinite-headland-60692.herokuapp.com/api`,
     TOKEN_KEY: 'recipenest-client-auth-token'
 }
 
 export default config
 
 
-//
+//API_ENDPOINT: `http://localhost:8000/api`,
 
-//API_ENDPOINT: `https://infinite-headland-60692.herokuapp.com/api`,
+//
