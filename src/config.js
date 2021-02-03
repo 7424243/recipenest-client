bcff94a58bb3770081e2c9cab61e9fd1a62c9453
Sam Sheets <sam.sheets94@gmail.com>
2021-02-03 16:44:40 -0700

    removed local host api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 const config = {
-    //API_ENDPOINT: `http://localhost:8000/api`,
     API_ENDPOINT: `https://infinite-headland-60692.herokuapp.com/api`,
     TOKEN_KEY: 'recipenest-client-auth-token'
 }
